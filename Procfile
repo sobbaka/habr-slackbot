94c7af3fee7ebcff3f0bc945ac0b7332077b9c33
sobbaka <a.bagaev1989@gmail.com>
2021-03-30 22:11:16 +0300

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn slackclient.wsgi --log-file -
--- slackclient/settings.py
+++ slackclient/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Celery
 CELERY_BROKER_URL = 'redis://localhost:6379'
